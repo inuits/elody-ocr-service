e7a9af4ccb3e2c47d339ce768bd41eb8ab3b506a
Jonas Verschueren <jonasv@inuits.eu>
2023-03-27 16:16:55 +0200

    Add extra swagger documentation

--- api/resources/status.py
+++ api/resources/status.py
@@ -9,7 +9,10 @@ class Status(Resource):
         self.headers = {"Authorization": f'Bearer {os.getenv("STATIC_JWT")}'}
 
     def get(self):
-        tesseract = os.popen("tesseract -v").read().split("\n")[0]
-        pytesseract = pkg_resources.working_set.by_key["pytesseract"].version
-        output = f"{tesseract} available\npytesseract {pytesseract} available"
-        return Response(response=output, status=200, mimetype="text/plain")
+        try:
+            tesseract = os.popen("tesseract -v").read().split("\n")[0]
+            pytesseract = pkg_resources.working_set.by_key["pytesseract"].version
+            output = f"{tesseract} available\npytesseract {pytesseract} available"
+            return Response(response=output, status=200, mimetype="text/plain")
+        except Exception as ex:
+            abort(status=400, message=str(ex))
